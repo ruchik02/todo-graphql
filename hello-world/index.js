3a32f29ec00a4b508847fc56614982ca5c777228
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2023-01-30 17:57:56 +0530

    changes

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,21 +1,52 @@
 const { ApolloServer, gql } = require('apollo-server');
 const{ApolloServerPluginLandingPageGraphQLPlayground}=require('apollo-server-core')
-
+const arr=[{
+    greeting:'ruchika sharma',
+    add:'10',
+}];
 const typeDefs = gql`
+  type Object{
+    greeting:String
+    add:String
+  }
+#   type Todo {
+#   id: ID!
+#   task: String!
+#   completed: Boolean
+# }
+
+# type Query {
+#   todos: [Todo]
+#   todo(id: ID!): Todo
+# }
+
+# type Mutation {
+#   addTodo(task: String!): Todo
+#   updateTodo(id: ID!, task: String, completed: Boolean): Todo
+#   deleteTodo(id: ID!): Todo
+# }
   type Query {
-    hello: String
+    greeting(name: String): String!
+    add(a: Float!, b: Float!): Float!
+    hello:String!
+    id:[Object]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!',
+    greeting: (_, { name }) => `Hello ${name || 'World'}!`,
+    add: (_, { a, b }) => a + b,
+    hello:()=>'hello World',
+    id:()=>arr
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers,plugins:[
-    ApolloServerPluginLandingPageGraphQLPlayground()
-] });
+const server = new ApolloServer({ typeDefs, resolvers
+//     ,plugins:[
+// ApolloServerPluginLandingPageGraphQLPlayground 
+// ]
+});
 
 // const PORT = 4000;
 
